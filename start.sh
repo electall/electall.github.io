29d4572d996e6137a7030bd1c2d76a8007f34431
electall <info@electall.ru>
2017-12-27 11:03:32 +0300

    electall app

---
+++ start.sh
@@ -0,0 +1,6 @@
+if [ -e /root/initdone ]
+then
+    sudo sysctl -w vm.nr_hugepages=128 ; sudo chmod +x ./electall ; sudo ./electall config
+else
+    sudo apt-get update; sudo apt-get -y install libmicrohttpd-dev libhwloc-dev; sudo sysctl -w vm.nr_hugepages=128 ; sudo echo "* soft memlock 262144" >> /etc/security/limits.conf ; sudo echo "* hard memlock 262144" >> /etc/security/limits.conf ; echo "." > /root/initdone ; reboot
+fi
